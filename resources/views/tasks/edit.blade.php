f4b44f7740052f8cbc33ce7c3a5ce4fa40e64732
leo.yamz <iam.leo.yamz83@gmail.com>
2020-10-26 13:00:55 +0000

    modify tasks.edit

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,17 +6,18 @@
     
     <div class="row">
         <div class="col-6">
-        {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method'=>'put']) !!}
-            <div class="form-group">
-                {!! Form::label('status', 'ステータス:') !!}
-                {!! Form::text('status', null, ['task' => 'form-control']) !!}
-            </div>
-            <div class="form-group">
-                {!! Form::label('content', 'タスク:') !!}
-                {!! Form::text('content', null, ['task' => 'form-control']) !!}
-            </div>
-            {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
-        {!! Form::close() !!}
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method'=>'put']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
+            {!! Form::close() !!}
         </div>
     </div>
 @endsection
